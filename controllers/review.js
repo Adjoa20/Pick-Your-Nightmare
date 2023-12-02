fcc20d58d9c008ba4eed52231476ef4e05b33641
agathaa <aamoako1120@gmail.com>
2023-12-01 22:20:01 -0500

    fixed the form for reviews

--- controllers/review.js
+++ controllers/review.js
@@ -14,24 +14,24 @@ router.get("", (req, res) => {
 });
 
 // DELETE
-router.delete("/:id", (req, res) => {
-  Review.findByIdAndRemove(req.params.id, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.redirect("/");
-    } else {
-      res.redirect("/");
-    }
-  });
+router.delete("/:id", async (req, res) => {
+    try {
+    const deleteReview = await Review.findByIdAndRemove(req.params.id);
+    res.redirect(`/movies/${deleteReview.category}`);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 
 // UPDATE
-router.put("/:id", (req, res) => {
-  const category = req.body.category;
-  Review.findByIdAndUpdate(req.params.id, req.body, { $set: req.body }, { new: true }, (err, updatedReviews) => {
-      res.redirect(`/movies/${category}`);
-    }
+router.put("/:id", async (req, res) => {
+  const updatedReview = await Review.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
   );
+  res.redirect(`/movies/${updatedReview.category}`);
 });
 
 // CREATE
@@ -50,7 +50,7 @@ router.post("", (req, res) => {
 // EDIT
 router.get("/:id/edit", (req, res) => {
   Review.findById(req.params.id, (err, foundReviews) => {
-    res.render("edit.ejs", { movie: foundReview });
+    res.render("edit.ejs", { review: foundReviews });
   });
 });
 
--- models/review.js
+++ models/review.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const reviewSchema = new mongoose.Schema({
     review: {type: String, required: true},
     // attachted to movie, easy to grab in the front end, make it easier to filter out
-    movie: {type: mongoose.Schema.Types.ObjectId, ref: 'Movie'},
     name: {type: String, required: true},
     category: {type: String, required: true}
 })
